c1e3719306f9cdde2ee87eaf57144e4e47927647
Sameer Maurya <sameermaurya97044@gmail.com>
2025-08-29 22:42:16 +0530

    all setup configured

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -16,6 +16,14 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
+// Routes import
+
+import userRouter from './routes/user.routes.js';
+
+// Routes Declaration
+app.use("/api/v1/users", userRouter)
+
+
 
 
 
