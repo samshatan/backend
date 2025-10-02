31f80fe1aaee3b87f3e6f27a8b25cdb0a557cdca
Sameer Maurya <sameermaurya97044@gmail.com>
2025-10-02 14:55:41 +0530

    add comment playlist tweets models

--- Backend/src/utils/cloudinary.js
+++ Backend/src/utils/cloudinary.js
@@ -16,10 +16,10 @@ const uploadOnCloudinary = async (localFilePath) =>{
     // file has been uploaded succesfully
     console.log("File has uploaded successfully on cloudinary");
     console.log(response);
-    // fs.unlinkSync(localFilePath);
+    fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
-    // fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
+    fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
     return null;
   }
 }
